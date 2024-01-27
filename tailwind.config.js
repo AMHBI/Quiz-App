065e5995763fba6cccce585fce2386de7d80857f
AMHBI <alimhbi1@gmail.com>
2024-01-27 19:02:39 +0330

    fix tailwind content bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.html", "./src/js/*.js"],
+  content: ["./*.html", "./js/*.js"],
   theme: {
     extend: {
       backgroundImage: {
